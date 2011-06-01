0a649114ddfaa1620e8df3e4fd338a1e319da4cf
Ritesh Sinha <r@ritesh-sinha.com>
2011-06-02 00:41:45 +0100

    Changed some crap

--- search/views.py
+++ search/views.py
@@ -1,7 +1,13 @@
 # Create your views here.
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
+#Process the default request, returns the index page
 def index(request):
-    return render_to_response('search/index.html')
+    return render_to_response('search/index.html',
+            context_instance=RequestContext(request))
+def results(request):
+    return render_to_response('search/results.html',
+            context_instance=RequestContext(request))
 
 
