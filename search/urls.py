8bfb0450465a554bc0db70a262f1515f8323eb26
Ritesh Sinha <r@ritesh-sinha.com>
2011-06-07 10:26:57 +0100

    Added rudimentary result page

--- search/urls.py
+++ search/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import *
 urlpatterns = patterns('search.views',
        (r'^$', 'index'),
+       (r'^search$', 'search'),
+       (r'^results$', 'results'),
        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
            {'document_root':'/Users/ritesh/code/django_static'}),
 )
